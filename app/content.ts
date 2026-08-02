12f0b5a06e59b2a6468ae84645bd084c28fe191b
Mohammad keshavarz <mohamad.persian1377@gmail.com>
2026-08-02 23:16:16 +0330

    Update content.ts

--- app/content.ts
+++ app/content.ts
@@ -53,8 +53,8 @@ export const profile: Profile = {
 
 export const currentFocus: string[] = [
   "Orchestration",
+  "Flow Design",
   "SAP & ABAP",
-  "Design systems",
 ];
 
 export const workExperiences: WorkExperience[] = [
@@ -152,7 +152,7 @@ export const posts: JournalPost[] = [
   // },
 ];
 
-export const skills: string[] = ["Flow design", "Product thinking", "C#", "Sql Server", "Blazor"];
+export const skills: string[] = ["System design", "Product thinking", "C#", "Sql Server", "Blazor"];
 
 export const professionalReading: ProfessionalBook[] = [
   // { title: "The Staff Engineer's Path", author: "Tanya Reilly", status: "Reading now", progress: "68%" },
